64fc917f21cc3096e5ee5ee0a0346dc05fd6000f
andikanatha <setya.natha@gmail.com>
2023-07-29 01:07:54 +0700

    feat [AN] add seeder tracker 7 month

--- seeders/20230728160007-moodTracker.js
+++ seeders/20230728160007-moodTracker.js
@@ -2,34 +2,40 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Moods', [
-      {
-        id: 1,
-        idUser: 2,
-        monthYear: '2023-07-28',
-        mood: 'happy',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ],{})
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    // Function to generate random moods
+    function getRandomMood() {
+      const moods = ['happy', 'normal', 'sad', 'angry'];
+      const randomIndex = Math.floor(Math.random() * moods.length);
+      return moods[randomIndex];
+    }
+
+    // Utility function to get date for a specific month and day
+    function getDateForMonthAndDay(month, day) {
+      const year = 2023; // Change the year if needed
+      return new Date(year, month - 1, day);
+    }
+
+    const months = 7; // January to June
+    const daysInMonth = [31, 28, 31, 30, 31, 30, 28]; // Number of days in each month
+    const idUser = 2; // User ID (update it with the appropriate ID)
+
+    for (let month = 1; month <= months; month++) {
+      for (let day = 1; day <= daysInMonth[month - 1]; day++) {
+        await queryInterface.bulkInsert('Moods', [
+          {
+            idUser,
+            monthYear: `2023-${month < 10 ? '0' + month : month}-${day < 10 ? '0' + day : day}`,
+            mood: getRandomMood(),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        ]);
+      }
+    }
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    // Add commands to revert seed here if needed.
+  },
 };
